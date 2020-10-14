c3672ec7266f0282f4f57db2ad7d4d3744de7b4d
Roshan Nizar <it17048774@my.sliit.lk>
2020-10-14 13:45:27 +0530

    testing

--- App.js
+++ App.js
@@ -1,10 +1,21 @@
-import React, {Component} from 'react';
-import {WebView} from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 
-class App extends Component {
-  render() {
-    return <WebView source={{uri: 'https://www.google.lk'}} />;
-  }
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
 }
 
-export default App;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
